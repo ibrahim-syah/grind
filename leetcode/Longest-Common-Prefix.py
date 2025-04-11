a5f53e83d8a1f7720c70430c31fbaba11492f42a
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-11 21:41:15 +0700

    [14] [Time Beats: 28.07%] [Memory Beats: 78.80%] - LeetPush

--- leetcode/Longest-Common-Prefix.py
+++ leetcode/Longest-Common-Prefix.py
@@ -1,21 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) <= 0:
-            return \\
+        res = \\
 
-        shortestWordLength = math.inf
-        for word in strs:
-            shortestWordLength = min(shortestWordLength, len(word))
+        for i in range(len(strs[0])):
+            for s in strs:
+                if len(s) == i or s[i] != strs[0][i]:
+                    return strs[0][:i]
 
-        if shortestWordLength <= 0:
-            return \\
+        return strs[0]
+        
 
-        result = \\
-        for i in range(shortestWordLength):
-            firstWord = strs[0][:i+1]
-            for word in strs:
-                if word[:i+1] != firstWord:
-                    return result
-            result = firstWord
-
-        return result
