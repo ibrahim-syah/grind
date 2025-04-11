bbe8c232998a940295714d43cc4b25b897e7a53a
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-11 20:14:21 +0700

    [242] [Time Beats: 43.37%] [Memory Beats: 97.45%] - LeetPush

--- leetcode/Valid-Anagram.py
+++ leetcode/Valid-Anagram.py
@@ -2,11 +2,10 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-            
-        scount = {}
-        tcount = {}
+
+        smap, tmap = {}, {}
         for i in range(len(s)):
-            scount[s[i]] = scount.get(s[i], 0) + 1
-            tcount[t[i]] = tcount.get(t[i], 0) + 1
-        
-        return scount == tcount
+            smap[s[i]] = smap.get(s[i], 0) + 1
+            tmap[t[i]] = tmap.get(t[i], 0) + 1
+
+        return smap == tmap
