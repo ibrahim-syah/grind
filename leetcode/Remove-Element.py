2e47c8bef14abe14fc934e39c48a511eaa1dd13d
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-12 08:13:05 +0700

    [27] [Time Beats: 100.00%] [Memory Beats: 52.99%] - LeetPush

--- leetcode/Remove-Element.py
+++ leetcode/Remove-Element.py
@@ -1,13 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        originalLen = len(nums)
         k = 0
-        for i in range(originalLen):
-            while nums[i] == val:
-                nums[i] = \_\
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k], nums[i] = nums[i], nums[k]
                 k += 1
-                nums[i], nums[originalLen-k] = nums[originalLen-k], nums[i]
 
-        return originalLen-k
-
-            
+        return k
