d771c51b2a25bafbbe2b2211616deeeff51989f8
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-14 08:38:31 +0700

    [217] [Time Beats: 85.01%] [Memory Beats: 54.11%] - LeetPush

--- leetcode/Contains-Duplicate.py
+++ leetcode/Contains-Duplicate.py
@@ -1,10 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        cache = set()
-
-        for i in range(len(nums)):
-            if nums[i] in cache:
+        numsSet = set()
+        for n in nums:
+            if n in numsSet:
                 return True
             else:
-                cache.add(nums[i])
-        return False 
+                numsSet.add(n) 
+        return False
