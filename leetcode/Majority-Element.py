16913db1be5c91f5d7f4b201a70bdd0e091a2083
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-15 20:52:22 +0700

    [169] [Time Beats: 17.69%] [Memory Beats: 19.95%] - LeetPush

--- leetcode/Majority-Element.py
+++ leetcode/Majority-Element.py
@@ -1,14 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        length = len(nums)
-        hashmap = defaultdict(int)
-        res = maxCount = 0
+        count = defaultdict(int)
+        res = nums[0]
+        maxCount = 0
+
         for n in nums:
-            hashmap[n] += 1
-            if hashmap[n] > length / 2:
-                return n
-            # if hashmap[n] > maxCount:
-            #     maxCount = hashmap[n]
-            #     res = n
+            count[n] += 1
+            if count[n] > maxCount:
+                res = n
+                maxCount = count[n]
         return res
-        
