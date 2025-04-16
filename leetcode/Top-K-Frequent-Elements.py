6a3feafd2133214189f7b964bc054798fff30f2f
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-16 08:45:17 +0700

    [347] [Time Beats: 13.28%] [Memory Beats: 13.35%] - LeetPush

--- leetcode/Top-K-Frequent-Elements.py
+++ leetcode/Top-K-Frequent-Elements.py
@@ -1,16 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        countmap = {}
-        bucket = [[] for i in range(len(nums) + 1)]
-
+        countmap = defaultdict(int)
         for n in nums:
-            countmap[n] = countmap.get(n, 0) + 1
-        
-        for n, v in countmap.items():
-            bucket[v].append(n)
-
+            countmap[n] += 1
+    
+        bucket = [[] for _ in range(len(nums)+1)]
+        for key, value in countmap.items():
+            bucket[value].append(key)
         res = []
-        for i in range(len(bucket)-1, 0, -1):
+        for i in range(len(nums), 0, -1):
             for n in bucket[i]:
                 res.append(n)
                 if len(res) == k:
