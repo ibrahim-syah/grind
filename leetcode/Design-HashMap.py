f4b72bf0cf28ba37d0cbad0d7d80f58669fdc1d9
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-20 20:06:42 +0700

    [706] [Time Beats: 20.37%] [Memory Beats: 26.37%] - LeetPush

--- leetcode/Design-HashMap.py
+++ leetcode/Design-HashMap.py
@@ -6,33 +6,33 @@ class ListNode:
 
 class MyHashMap:
     def __init__(self):
-        self.arr = [ListNode(0, 0) for i in range(10000)]
+        self.arr = [ListNode(0, -1) for _ in range(10000)]
 
     def put(self, key: int, value: int) -> None:
-        node = self.arr[key % len(self.arr)]
-        while node.next:
-            if node.next.key == key:
-                node.next.value = value
+        curr = self.arr[key % len(self.arr)]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.value = value
                 return
-            node = node.next
-        node.next = ListNode(key, value)
+            curr = curr.next
+        curr.next = ListNode(key, value)
+        
 
     def get(self, key: int) -> int:
-        node = self.arr[key % len(self.arr)]
-        while node.next:
-            if node.next.key == key:
-                return node.next.value
-            node = node.next
+        curr = self.arr[key % len(self.arr)]
+        while curr.next:
+            if curr.next.key == key:
+                return curr.next.value
+            curr = curr.next
         return -1
 
     def remove(self, key: int) -> None:
-        node = self.arr[key % len(self.arr)]
-        while node.next:
-            if node.next.key == key:
-                node.next = node.next.next
+        curr = self.arr[key % len(self.arr)]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
                 return
-            node = node.next
-
+            curr = curr.next
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
