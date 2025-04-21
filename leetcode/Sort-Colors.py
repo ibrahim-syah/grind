92309eb6aab93907f5d602c9a98b72d48d44097a
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-21 07:43:37 +0700

    [75] [Time Beats: 100.00%] [Memory Beats: 12.37%] - LeetPush

--- leetcode/Sort-Colors.py
+++ leetcode/Sort-Colors.py
@@ -1,16 +1,17 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        \\\
+        """
         Do not return anything, modify nums in-place instead.
-        \\\
-        occurences = [0] * 3
-
+        """
+        freq = defaultdict(int)
         for n in nums:
-            occurences[n] += 1
+            freq[n] += 1
 
-        n = 0
-        for i in range(3):
-            for j in range(occurences[i]):
-                nums[n] = i
-                n += 1
+        i = 0
+        color = 0
+        while i < len(nums):
+            for c in range(freq[color]):
+                nums[i] = color
+                i += 1
+            color += 1
         
