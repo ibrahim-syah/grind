71d46a5d18fc73d0fc3dbeb5911a3c4f88fca56a
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-22 20:49:02 +0700

    [1] [Time Beats: 58.75%] [Memory Beats: 24.86%] - LeetPush

--- leetcode/Two-Sum.py
+++ leetcode/Two-Sum.py
@@ -1,11 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        complementmap = {}
+        hashmap = {}
 
         for i in range(len(nums)):
             complement = target - nums[i]
-            if complement in complementmap:
-                return [i, complementmap[complement]]
-            else:
-                complementmap[nums[i]] = i
-        
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[nums[i]] = i
