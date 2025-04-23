39298210a6bea28813dc23a7be3753d7420f031a
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-23 08:36:23 +0700

    [36] [Time Beats: 83.61%] [Memory Beats: 70.06%] - LeetPush

--- leetcode/Valid-Sudoku.py
+++ leetcode/Valid-Sudoku.py
@@ -4,16 +4,15 @@ class Solution:
         cols = defaultdict(set)
         squares = defaultdict(set)
 
-        for i in range(9):
-            for j in range(9):
-                curr = board[i][j]
-                if curr == \.\:
+        for r in range(9):
+            for c in range(9):
+                curr = board[r][c]
+                if curr == ".":
                     continue
-
-                if curr in rows[i] or curr in cols[j] or curr in squares[(i//3, j//3)]:
+                if curr in rows[r] or curr in cols[c] or curr in squares[(r//3,c//3)]:
                     return False
-                rows[i].add(curr)
-                cols[j].add(curr)
-                squares[(i//3, j//3)].add(curr)
-
+                rows[r].add(curr)
+                cols[c].add(curr)
+                squares[(r//3,c//3)].add(curr)
         return True
+        
