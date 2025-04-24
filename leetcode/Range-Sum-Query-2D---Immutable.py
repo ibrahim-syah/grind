ffe94eef05cbae45a502fa672aac40960aab2760
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-24 07:41:17 +0700

    [304] [Time Beats: 73.12%] [Memory Beats: 32.92%] - LeetPush

--- leetcode/Range-Sum-Query-2D---Immutable.py
+++ leetcode/Range-Sum-Query-2D---Immutable.py
@@ -1,20 +1,21 @@
 class NumMatrix:
+
     def __init__(self, matrix: List[List[int]]):
-        rows, cols = len(matrix), len(matrix[0])
-        self.cache = [[0 for _ in range(cols + 1)] for _ in range(rows + 1)]
+        rows = len(matrix)
+        cols = len(matrix[0])
+        self.cache = [([0] * (cols + 1)) for _ in range(rows + 1)]
         for r in range(rows):
-            rowPrefix = 0
+            prefix = 0
             for c in range(cols):
-                rowPrefix += matrix[r][c]
+                prefix += matrix[r][c]
                 above = self.cache[r][c+1]
-                self.cache[r+1][c+1] = rowPrefix + above
+                self.cache[r+1][c+1] = prefix + above
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        r1, c1, r2, c2 = row1+1, col1+1, row2+1, col2+1
-        full = self.cache[r2][c2]
-        above = self.cache[r1-1][c2]
-        left = self.cache[r2][c1-1]
-        topleft = self.cache[r1-1][c1-1]
+        full = self.cache[row2+1][col2+1]
+        above = self.cache[row1][col2+1]
+        left = self.cache[row2+1][col1]
+        topleft = self.cache[row1][col1]
         return full - above - left + topleft
         
 
