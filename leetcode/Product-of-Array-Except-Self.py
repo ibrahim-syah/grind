7051a1b3ae8f000807c471ded2de007c9a59f038
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-24 08:11:02 +0700

    [238] [Time Beats: 48.00%] [Memory Beats: 11.64%] - LeetPush

--- leetcode/Product-of-Array-Except-Self.py
+++ leetcode/Product-of-Array-Except-Self.py
@@ -2,19 +2,16 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         product = 1
         prefix = [0] * len(nums)
-        for i in range(len(nums)):
+        for i in range(len(prefix)):
             prefix[i] = product
             product *= nums[i]
-
-        product = 1
         suffix = [0] * len(nums)
-        for i in range(len(nums)-1, -1, -1):
+        product = 1
+        for i in range(len(suffix)-1, -1, -1):
             suffix[i] = product
             product *= nums[i]
-
+        
         res = [0] * len(nums)
         for i in range(len(nums)):
             res[i] = prefix[i] * suffix[i]
-
         return res
-            
