9f670bad56c7c28c2a42981f780be1567b47fdd7
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-28 08:03:33 +0700

    [36] [Time Beats: 39.97%] [Memory Beats: 25.24%] - LeetPush

--- leetcode/Valid-Sudoku.py
+++ leetcode/Valid-Sudoku.py
@@ -1,18 +1,14 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        squares = defaultdict(set)
-
+        rows, cols, squares = defaultdict(set), defaultdict(set), defaultdict(set)
         for r in range(9):
             for c in range(9):
-                curr = board[r][c]
-                if curr == ".":
+                char = board[r][c]
+                if  char == ".":
                     continue
-                if curr in rows[r] or curr in cols[c] or curr in squares[(r//3,c//3)]:
+                if char in rows[r] or char in cols[c] or char in squares[(r//3, c//3)]:
                     return False
-                rows[r].add(curr)
-                cols[c].add(curr)
-                squares[(r//3,c//3)].add(curr)
+                rows[r].add(char)
+                cols[c].add(char)
+                squares[(r//3, c//3)].add(char)
         return True
-        
