78fc701c4c1714b17472afe4cb8402182bc4be7f
ibrahim-syah <76517617+ibrahim-syah@users.noreply.github.com>
2025-04-30 07:47:43 +0700

    [49] [Time Beats: 19.04%] [Memory Beats: 31.43%] - LeetPush

--- leetcode/Group-Anagrams.py
+++ leetcode/Group-Anagrams.py
@@ -1,9 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group = defaultdict(list)
+        groups = defaultdict(list)
+
         for s in strs:
             count = [0] * 26
             for c in s:
-                count[ord(c) - ord("a")] += 1
-            group[tuple(count)].append(s)
-        return list(group.values())
+                count[ord(c)-ord("a")] += 1
+            groups[tuple(count)].append(s)
+        return list(groups.values())
